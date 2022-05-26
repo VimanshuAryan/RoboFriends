d0686b89f3057d7fefedf8d47c58e9a9e980aeed
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-05-26 15:02:20 +0530

    Updates

---
+++ precache-manifest.a9c94023e639fd91deb864946b6d69a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1839a838e45ae155bb6",
+    "url": "/RoboFriends/static/css/main.3f23f48a.chunk.css"
+  },
+  {
+    "revision": "f1839a838e45ae155bb6",
+    "url": "/RoboFriends/static/js/main.f1839a83.chunk.js"
+  },
+  {
+    "revision": "eb604781108bd0ef28f0",
+    "url": "/RoboFriends/static/css/1.77544e27.chunk.css"
+  },
+  {
+    "revision": "eb604781108bd0ef28f0",
+    "url": "/RoboFriends/static/js/1.eb604781.chunk.js"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a2f128315c670f782b533137c75c1acc",
+    "url": "/RoboFriends/index.html"
+  }
+];
